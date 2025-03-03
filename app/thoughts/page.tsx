4a509888b79036aca8b09ac95a92dbd53c170e5b
Arjun C Unni <arjuncrevathi@gmail.com>
2025-03-03 10:16:47 +0400

    removed thoughts and added blog

--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -1,12 +1,10 @@
 import RiskMeasures from "./components/RiskMeasures";
 import Quotes from "@/app/components/Quotes";
 import HeroThoughts from "./components/HeroThoughts";
-import Blogs from "./components/blogs";
 export default function About() {
   return (
     <>
         <HeroThoughts />
-        <Blogs />
          <RiskMeasures />
         <Quotes />
 
